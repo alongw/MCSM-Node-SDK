788ffe5ac00b08847b29a19328592a0174ee4125
alongw <alongw@alongw.cn>
2024-09-27 00:49:48 +0800

    feat: parse config

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,7 +4,7 @@ import type { MCSM_CONSTRUCTOR_CONFIG } from '@/types/index'
 export const parseConfig = (config: MCSM_CONSTRUCTOR_CONFIG) => {
     const newConfig: MCSM_CONSTRUCTOR_CONFIG = {
         api_key: _.trim(config.api_key),
-        panel_url: _.chain(config.panel_url).trimEnd('/').value()
+        panel_url: _.chain(config.panel_url).trim().trimEnd('/').value()
     }
 
     return newConfig
