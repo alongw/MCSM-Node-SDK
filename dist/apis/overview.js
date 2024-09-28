e5da63b00c58abe77b972840d3a77ecb3772ccf7
alongw <alongw@alongw.cn>
2024-09-28 17:25:40 +0800

    feat: update package.json

--- dist/apis/overview.js
+++
@@ -1,4 +0,0 @@
-export const getOverview = (request) => {
-    return request.get('/overview');
-};
-//# sourceMappingURL=overview.js.map
--- dist/types/daemon.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=daemon.js.map
